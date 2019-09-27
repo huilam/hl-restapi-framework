c109f6380a1ec00a47d8b6a68b26c8768e4fffaf
onghuilam <onghuilam@gmail.com>
2019-09-27 14:02:14 +0800

    update to latest hl-common

--- src/hl/restapi/service/RESTApiException.java
+++ src/hl/restapi/service/RESTApiException.java
@@ -22,181 +22,23 @@
 
 package hl.restapi.service;
 
-public class RESTApiException extends Exception {
+import hl.common.CommonException;
 
-	private static final long serialVersionUID = -1068899117288657750L;
-	private String error_code 		= null;
-	private String error_subject 	= null;
-	private String error_reason 	= null;
-	
-	private String error_debuginfo 	= null;
-	
-	private Throwable throwable 	= null;
-	private String throwable_cause 	= null;
-	
-	public static boolean debug_mode = false;
+public class RESTApiException extends CommonException {
 
-	public RESTApiException(String aErrCode, String aErrReason)
-	{
-		super(aErrReason);
-		init(aErrCode, aErrReason, null, null);
+	public RESTApiException(String aErrCode, String aErrReason, String aErrDebugInfo, Throwable aThrowable) {
+		super(aErrCode, aErrReason, aErrDebugInfo, aThrowable);
 	}
 	
-	public RESTApiException(String aErrCode, String aErrReason, Throwable aThrowable)
-	{
-		super(aErrReason, aThrowable);
-		init(aErrCode, aErrReason, null, aThrowable);
-	}
-	
-	public RESTApiException(String aErrCode, String aErrReason, String aErrDebugInfo, Throwable aThrowable)
-	{
-		super(aErrReason, aThrowable);
-		init(aErrCode, aErrReason, aErrDebugInfo, aThrowable);
-	}
-	
-	
-	public RESTApiException(String aErrCode, Throwable aThrowable)
-	{
-		super(aThrowable);
-		init(aErrCode, null, null, aThrowable);
-	}
-	
-	public void setErrSubjectAndReason(String aErrCode, String aErrSubject, String aErrReason)
-	{
-		this.error_code 	= aErrCode;
-		this.error_subject 	= aErrSubject;
-		this.error_reason 	= aErrReason; 
-	}
-	
-	public String getErrorSubject()
-	{
-		return this.error_subject;
-	}
-	
-	public void setErrorSubject(String aErrSubject)
-	{
-		this.error_subject = aErrSubject;
+	public RESTApiException(String aErrCode, String aErrReason, Throwable aThrowable) {
+		super(aErrCode, aErrReason, aThrowable);
 	}
 
-
-	public String getErrorReason()
-	{
-		return this.error_reason;
-	}
-
-	public void setErrorReason(String aErrReason)
-	{
-		this.error_reason = aErrReason;
-	}
-
-	private void init(String aErrCode, String aErrReason, String aErrDebugInfo, Throwable aThrowable)
-	{
-		error_code = aErrCode;
-		throwable_cause = getCauseErrMsg(aThrowable);
-		error_reason = aErrReason;
-		error_debuginfo = aErrDebugInfo;
-		throwable = aThrowable;
-		
-		if(error_reason==null)
-		{
-			error_reason = throwable_cause;
-			throwable_cause = null;
-		}
-		
-		if(aThrowable!=null)
-		{
-			aThrowable.printStackTrace();
-		}
-	}
-
-	
-	public String getErrorCode()
-	{
-		return error_code;
+	public RESTApiException(String aErrCode, Throwable aThrowable) {
+		super(aErrCode, aThrowable);
 	}
 	
-	public String getErrorMsg()
-	{
-		StringBuffer sb = new StringBuffer();
-		
-		if(getErrorSubject()!=null)
-		{
-			sb.append(getErrorSubject());
-		}
-		
-		if(getErrorReason()!=null)
-		{
-			if(sb.length()>0)
-				sb.append(" - ");
-			sb.append(getErrorReason());
-		}
-		
-		if(sb.length()==0 && throwable!=null)
-		{
-			sb.append(throwable.getMessage());
-		}
-		
-		return sb.toString();
-	}
-	
-	public Throwable getThrowable()
-	{
-		return throwable;
-	}
-	
-	public String getErrorCause()
-	{
-		return throwable_cause;
-	}
-	
-	public String getErrorDebugInfo()
-	{
-		return error_debuginfo;
-	}
-	
-	public void setErrorCause(String aErrorCause)
-	{
-		throwable_cause = aErrorCause;
-	}
-	
-	public void setErrorDebugInfo(String aErrorDebugInfo)
-	{
-		error_debuginfo = aErrorDebugInfo;
-	}
-	
-	public void setDebugMode(boolean isDebug)
-	{
-		debug_mode = isDebug;
-	}
-
-	public String getMessage()
-	{
-		StringBuffer sbErr = new StringBuffer();
-		sbErr.append(getErrorMsg());
-		//
-		if(debug_mode)
-		{
-			sbErr.append("  ").append(getErrorDebugInfo());
-		}
-		return sbErr.toString();
-	}
-	
-	private String getCauseErrMsg(Throwable aThrowable)
-	{
-		if(aThrowable==null)
-			return null;
-		
-		if(aThrowable instanceof RESTApiException)
-		{
-			return ((RESTApiException)aThrowable).throwable_cause;
-		}
-		
-		Throwable t = aThrowable.getCause();
-		if(t!=null && t.getMessage()!=null)
-		{
-			return  t.getMessage();
-		}
-		
-		return aThrowable.getMessage();
+	public RESTApiException(String aErrCode, String aErrReason) {
+		super(aErrCode, aErrReason);
 	}
 }
